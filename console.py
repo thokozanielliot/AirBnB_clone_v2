519039e0dc2f541cf5c6d42084fbbbccbaa87322
Thokozani Elliot <thokozanielliot58@gmail.com>
2023-11-22 21:55:59 +0200

    Update console.py

--- console.py
+++ console.py
@@ -2,8 +2,12 @@
 """ Console Module """
 import cmd
 import sys
+import re
+import os
+import uuid
+from datetime import datetime
 from models.base_model import BaseModel
-from models.__init__ import storage
+from models import storage
 from models.user import User
 from models.place import Place
 from models.state import State
